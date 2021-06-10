25e177af0b841bdaf3d667236c529ddd2a1cf058
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-06-10 00:37:38 -0300

    update server config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
-// const path = require('path');
-const express = require('express');
-const cors = require('cors');
-// const { sequelize } = require('./config/database');
 const routes = require('./app/routes');
+const app = require('./config/server');
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-app.use(express.json());
-app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
