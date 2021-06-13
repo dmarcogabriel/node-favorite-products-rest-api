3ad46204b3703366fc8d738e4164ce32a21c4b79
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-06-13 17:26:45 -0300

    update routes with auth

--- src/app/routes/products.js
+++ src/app/routes/products.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const controller = require('../controllers/products');
+const authMiddleware = require('../middlewares/auth');
 
 const router = Router();
 
 router.get('/', controller.get);
-router.get('/:id', controller.getById);
-router.post('/:userId', controller.post);
+router.get('/:id', authMiddleware.authorize, controller.getById);
+router.post('/', authMiddleware.authorize, controller.post);
 
 module.exports = router;
--- src/app/routes/user.js
+++ src/app/routes/user.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const controller = require('../controllers/user');
+const authMiddleware = require('../middlewares/auth');
 
 const router = Router();
 
-router.get('/', controller.get);
+router.get('/', authMiddleware.authorize, controller.get);
+router.get('/:id', authMiddleware.authorize, controller.get);
 router.post('/', controller.post);
+router.put('/:id', authMiddleware.authorize, controller.put);
+router.delete('/:id', authMiddleware.authorize, controller.delete);
 
 module.exports = router;
