22ca2c2ce7f7cf912a03e5e3739ab92ef822b10c
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-06-13 17:26:11 -0300

    add auth routes to app

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const usersRoutes = require('./user');
 const productsRoutes = require('./products');
+const authRoutes = require('./auth.routes');
 
 const router = Router();
 
 router.use('/users', usersRoutes);
 router.use('/products', productsRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
