1abdca16aa4bd435eddf1b9cdcd06206d1e3c34e
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-06-13 21:17:57 -0300

    add user repository tests

---
+++ src/app/repository/__tests__/user.repository.test.js
@@ -0,0 +1,73 @@
+const userRepository = require('../users');
+
+const mockUser = {
+  name: 'User Tester',
+  email: 'user_tester_1@tester.com',
+  password: 'aaaaaa',
+};
+let mockUserId;
+
+describe('User repository', () => {
+  it('should pass on create user', async () => {
+    const user = await userRepository.create(mockUser);
+    mockUserId = user.id;
+
+    expect(user.name).toEqual(mockUser.name);
+    expect(user.id).toEqual(expect.any(Number));
+    expect(user.password).toEqual(expect.any(String));
+    expect(user.email).toEqual(mockUser.email);
+  });
+
+  it('should fail on create user with existing email', async () => {
+    try {
+      await userRepository.create(mockUser);
+    } catch (error) {
+      expect(error.message).toEqual('Email must be unique');
+    }
+  });
+
+  it('should pass on get users', async () => {
+    const users = await userRepository.find();
+    expect(users).toEqual(expect.any(Array));
+    expect(users).toHaveLength(1);
+  });
+
+  it('should pass on get user by id', async () => {
+    const user = await userRepository.findOne(mockUserId);
+
+    expect(user.name).toEqual(mockUser.name);
+    expect(user.id).toEqual(mockUserId);
+    expect(user.password).toEqual(expect.any(String));
+    expect(user.email).toEqual(mockUser.email);
+  });
+
+  it('should pass on update user', async () => {
+    const user = await userRepository.update(mockUserId, {
+      ...mockUser,
+      name: 'Updated Tester',
+    });
+    expect(user.name).toEqual('Updated Tester');
+  });
+
+  it('should fail on update user', async () => {
+    try {
+      await userRepository.update(mockUserId);
+    } catch (error) {
+      expect(error.message)
+        .toEqual(
+          'Cannot destructure property \'name\' of \'undefined\' as it is undefined.',
+        );
+    }
+  });
+
+  it('should pass on remove user', async () => {
+    await userRepository.remove(mockUserId);
+    const user = await userRepository.findOne(mockUserId);
+    expect(user).toEqual(null);
+  });
+
+  it('should fail on get user by id', async () => {
+    const user = await userRepository.findOne(mockUserId);
+    expect(user).toEqual(null);
+  });
+});
--- src/app/repository/users.js
+++ src/app/repository/users.js
@@ -1,8 +1,10 @@
-const Product = require('../models/Product');
-const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { Product } = require('../models');
+const { User } = require('../models');
 
-exports.create = async ({ name, email }) => {
-  const user = User.build({ name, email });
+exports.create = async ({ name, email, password }) => {
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync());
+  const user = User.build({ name, email, password: passwordHash });
   await user.save();
   return user;
 };
@@ -10,4 +12,19 @@ exports.create = async ({ name, email }) => {
 exports.find = async () => {
   const users = await User.findAll({ include: Product });
   return users;
+};
+
+exports.findOne = async id => {
+  const user = await User.findOne({ where: { id }, include: Product });
+  return user;
+};
+
+exports.update = async (id, { name, email }) => {
+  await User.update({ name, email }, { where: { id } });
+  const user = await User.findOne({ where: { id } });
+  return user;
+};
+
+exports.remove = async id => {
+  await User.destroy({ where: { id } });
 };
