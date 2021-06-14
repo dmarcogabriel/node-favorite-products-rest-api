c1285ba6bf5ce3e8a1c0a4ba61a39572a4b4dd96
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-06-14 11:11:24 -0300

    update database config

--- src/config/database.js
+++ src/config/database.js
@@ -5,5 +5,5 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  // logging: process.env.NODE_ENV !== 'test',
+  logging: process.env.NODE_ENV !== 'test',
 };
